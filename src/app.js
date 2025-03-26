d4325eb127ae946d4e808585f5683fc985dc13c6
RigelTLT <kozyrin-di@yandex.ru>
2025-03-26 16:13:59 +0400

    fix: rollback

--- src/app.js
+++ src/app.js
@@ -15,9 +15,9 @@ app.use("/api", apiRouter);
 
 // Serve frontend in production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../../frontend/public")));
+  app.use(express.static(path.join(__dirname, "../frontend/public")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../frontend/public/index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/public/index.html"));
   });
 }
 
