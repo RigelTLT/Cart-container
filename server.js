966d442d97507591367c57e70a251ada26657213
RigelTLT <kozyrin-di@yandex.ru>
2025-04-01 11:35:30 +0400

    fix: fix listen port

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
 // Авторизация Google Sheets
 async function getDoc() {
   const auth = new JWT({
